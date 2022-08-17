465f4de44dcfa62bf2ba9212d60d3b416a72eea1
kotu-kotuo <ttn-0223@mineo.jp>
2022-08-17 16:30:15 +0900

    ♻️ HTML文章列の表示 #7

--- src/pages/blog/[cid].tsx
+++ src/pages/blog/[cid].tsx
@@ -12,10 +12,12 @@ const BlogContent: NextPage = () => {
         </div>
         <div className="wrapper">
           <p className="my-1 text-sm font-bold text-gray-400">2022.07.11</p>
-          <p>
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-            sint. Velit officia consequat duis enim velit mollit.
-          </p>
+          <p
+            dangerouslySetInnerHTML={{
+              __html:
+                "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.Velit officia consequat duis enim velit mollit.",
+            }}
+          />
         </div>
       </Layout>
     </>
--- src/pages/portfolio/[cid].tsx
+++ src/pages/portfolio/[cid].tsx
@@ -17,18 +17,16 @@ const PortfolioContent: NextPage = () => {
             className="aspect-video w-full object-cover"
           />
           <div className="my-4 text-xl font-bold text-gray-900">IT KINGDOM</div>
-          <p className="my-2 text-gray-900">
-            当サロンのLPページ。React、Next.js、TypeScriptなどのモダンな技術を用いて作られています。初心者にちょうど良い難易度の制作物です。
-            <br />
-            <br />
-            使用技術とかこだわりのポイントとか書いてもいいかもですね
-            <br />
-            <br />
-            まっ白な、あのさっきの北の十字架のようにうつくしくかがやいて見えました。走るときはまるで鼠のような実もちらっと見えたのでした。青年はぞくっとしているのでした。
-          </p>
-          <p className="my-2 text-sm font-bold text-gray-400">
+          <p className="mb-4 text-xs font-bold text-gray-400">
             2021.10 - 2021.12
           </p>
+          <p
+            className="my-2 text-gray-900"
+            dangerouslySetInnerHTML={{
+              __html:
+                "当サロンのLPページ。React、Next.js、TypeScriptなどのモダンな技術を用いて作られています。初心者にちょうど良い難易度の制作物です。使用技術とかこだわりのポイントとか書いてもいいかもですね。まっ白な、あのさっきの北の十字架のようにうつくしくかがやいて見えました。走るときはまるで鼠のような実もちらっと見えたのでした。青年はぞくっとしているのでした。",
+            }}
+          />
         </div>
       </Layout>
     </>
