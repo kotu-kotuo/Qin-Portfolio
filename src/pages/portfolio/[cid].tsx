dec7a09e455609975c1aea0695cd5ba589a8cda7
kotu-kotuo <ttn-0223@mineo.jp>
2022-08-24 03:27:54 +0900

    ポートフォリオ詳細ページにmicroCMS反映

--- src/pages/portfolio/[cid].tsx
+++ src/pages/portfolio/[cid].tsx
@@ -1,8 +1,14 @@
-import { NextPage } from "next";
+import dayjs from "dayjs";
+import { MicroCMSContentId, MicroCMSDate } from "microcms-js-sdk";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import React from "react";
 import Headline from "src/components/Element/Headline";
+import { client } from "src/lib/client";
+import { Portfolio } from "src/pages";
 
-const PortfolioContent: NextPage = () => {
+type PortfolioProps = Portfolio & MicroCMSContentId & MicroCMSDate;
+
+const PortfolioContent: NextPage<PortfolioProps> = (props) => {
   return (
     <>
       <div className="headline-wrapper">
@@ -10,24 +16,53 @@ const PortfolioContent: NextPage = () => {
       </div>
       <div className="wrapper">
         <img
-          src="https://source.unsplash.com/random?q=60"
+          src={props.featuredImage.url}
           alt="画像"
           className="aspect-video w-full object-cover"
         />
-        <div className="my-4 text-xl font-bold text-gray-900">IT KINGDOM</div>
+        <div className="my-4 text-xl font-bold text-gray-900">
+          {props.title}
+        </div>
         <p className="mb-4 text-xs font-bold text-gray-400">
-          2021.10 - 2021.12
+          {dayjs(props.startDate).format("YYYY.MM")}{" "}
+          <span>{props.endDate ? "-" : "~"}</span>{" "}
+          {dayjs(props.endDate).format("YYYY.MM")}
         </p>
-        <p
+        <div
           className="my-2 text-gray-900"
           dangerouslySetInnerHTML={{
-            __html:
-              "当サロンのLPページ。React、Next.js、TypeScriptなどのモダンな技術を用いて作られています。初心者にちょうど良い難易度の制作物です。使用技術とかこだわりのポイントとか書いてもいいかもですね。まっ白な、あのさっきの北の十字架のようにうつくしくかがやいて見えました。走るときはまるで鼠のような実もちらっと見えたのでした。青年はぞくっとしているのでした。",
+            __html: props.content,
           }}
         />
       </div>
     </>
   );
+};
+
+export const getStaticPaths: GetStaticPaths<{ cid: string }> = async () => {
+  const data = await client.getList({ endpoint: "portfolio" });
+  const ids = data.contents.map((content) => `/portfolio/${content.id}`);
+  return {
+    paths: ids,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<
+  PortfolioProps,
+  { cid: string }
+> = async (ctx) => {
+  if (!ctx.params) {
+    return { notFound: true };
+  }
+  const data = await client.getListDetail<Portfolio>({
+    endpoint: "portfolio",
+    contentId: ctx.params.cid,
+  });
+
+  return {
+    props: data,
+  };
 };
 
 export default PortfolioContent;
