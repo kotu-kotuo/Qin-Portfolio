9443b97ccea3997659a108abee9d8a74780bcd0c
kotu-kotuo <ttn-0223@mineo.jp>
2022-08-25 01:20:16 +0900

    ✨ ブログリストにSSG, microCMSを導入

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,42 +1,39 @@
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 import React from "react";
 import BlogItem from "src/components/Item/BlogItem";
 import Headline from "src/components/Element/Headline";
+import { client } from "src/lib/client";
+import { Blog } from "src/pages";
+import { MicroCMSListResponse } from "microcms-js-sdk";
 
-const Blog: NextPage = () => {
+const Blog: NextPage<MicroCMSListResponse<Blog>> = (props) => {
   return (
     <>
       <div className="headline-wrapper">
         <Headline title="Blog" />
       </div>
-      <div className="md:mx-auto md:max-w-screen-md">
-        {/* <div className="item-wrapper">
-          <BlogItem />
-        </div>
-        <div className="item-wrapper">
-          <BlogItem />
-        </div>
-        <div className="item-wrapper">
-          <BlogItem />
-        </div>
-        <div className="item-wrapper">
-          <BlogItem />
-        </div>
-        <div className="item-wrapper">
-          <BlogItem />
-        </div>
-        <div className="item-wrapper">
-          <BlogItem />
-        </div>
-        <div className="item-wrapper">
-          <BlogItem />
-        </div>
-        <div className="item-wrapper">
-          <BlogItem />
-        </div> */}
-      </div>
+      <ul className="list-none pl-0 md:mx-auto  md:max-w-screen-md">
+        {props.contents.map((content) => (
+          <li className="item-wrapper" key={content.id}>
+            <BlogItem
+              contentID={content.id}
+              title={content.title}
+              content={content.content}
+              publishedAt={content.publishedAt}
+            />
+          </li>
+        ))}
+      </ul>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const data = await client.getList<Blog>({ endpoint: "blog" });
+
+  return {
+    props: data,
+  };
 };
 
 export default Blog;
