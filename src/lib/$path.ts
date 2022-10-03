60a0a15c991d34df9e9821a2bb46aa5712eeebec
kotu-kotuo <ttn022300@gmail.com>
2022-10-03 22:45:39 +0900

    ディレクトリ構成少し変更

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -3,19 +3,19 @@ export const pagesPath = {
     $url: (url?: { hash?: string }) => ({ pathname: '/about' as const, hash: url?.hash })
   },
   "blog": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/blog' as const, hash: url?.hash }),
     _cid: (cid: string | number) => ({
       $url: (url?: { hash?: string }) => ({ pathname: '/blog/[cid]' as const, query: { cid }, hash: url?.hash })
-    })
+    }),
+    $url: (url?: { hash?: string }) => ({ pathname: '/blog' as const, hash: url?.hash })
   },
   "contact": {
     $url: (url?: { hash?: string }) => ({ pathname: '/contact' as const, hash: url?.hash })
   },
   "portfolio": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/portfolio' as const, hash: url?.hash }),
     _cid: (cid: string | number) => ({
       $url: (url?: { hash?: string }) => ({ pathname: '/portfolio/[cid]' as const, query: { cid }, hash: url?.hash })
-    })
+    }),
+    $url: (url?: { hash?: string }) => ({ pathname: '/portfolio' as const, hash: url?.hash })
   },
   $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
 }
