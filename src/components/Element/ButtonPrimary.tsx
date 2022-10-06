8280457622c8aea93cef4bcf6be6baefe7d28b8d
kotu-kotuo <ttn022300@gmail.com>
2022-10-06 15:58:14 +0900

    ボタンにtarget_blank追加&名前変更

--- src/components/Element/ButtonPrimary.tsx
+++ src/components/Element/ButtonPrimary.tsx
@@ -9,11 +9,13 @@ type ButtonProps = {
   type?: "button" | "submit" | "reset";
 };
 
-const ButtonBlack: FC<ButtonProps> = ({ text, link, externalLink, type }) => {
+const ButtonPrimary: FC<ButtonProps> = ({ text, link, externalLink, type }) => {
   return externalLink ? (
     <Button
       className="rounded-full bg-gray-900 font-button text-white no-underline"
       href={link}
+      target="_blank"
+      rel="noopener noreferrer"
       component="a"
     >
       {text}
@@ -31,4 +33,4 @@ const ButtonBlack: FC<ButtonProps> = ({ text, link, externalLink, type }) => {
   );
 };
 
-export default ButtonBlack;
+export default ButtonPrimary;
